7cb00e6764460bfbed6158a72828cf6a3bfc851b
mrozikdawid <mrozik.dawid@gmail.com>
2017-10-19 14:34:57 +0200

    Add files via upload

---
+++ cwiczenia12102017/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cwiczenia12102017
+{
+    class Car
+    {
+        public string marka;
+        public int rok;
+        private string model;
+        private int iloscDrzwi;
+        private int pojemnoscSilnika;
+        public double srednieSpalanie;
+
+
+        public void WypiszInfo()
+        {
+           Console.WriteLine("marka i rok produkcji: {0} {1} ", marka ,rok);
+        }
+
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            return srednieSpalanie * dlugoscTrasy;
+        }
+
+        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+        {
+            return ObliczSpalanie(dlugoscTrasy) * cenaPaliwa;
+        }
+            
+
+           
+       
+        
+           
+            
+        
+    }
+}
---
+++ cwiczenia12102017/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cwiczenia12102017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carName = "Moj Samochod";
+            Console.WriteLine(carName);
+
+            Car car1 = new Car();
+            car1.marka = "Ford";
+            car1.rok = 1993;
+            car1.WypiszInfo();
+            car1.srednieSpalanie = 8;
+            /*Car car2 = new Car();
+            car2.marka = "BMW";
+            car2.rok = 2015;
+            car2.WypiszInfo();
+            car1 = car2;
+            car2.WypiszInfo();
+            car1.WypiszInfo();*/
+
+            Console.WriteLine(car1.ObliczKosztPrzejazdu(100, 8));
+            
+
+            Console.ReadKey();
+        }
+    }
+}
